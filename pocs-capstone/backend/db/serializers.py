f9a20ab5c06dc224f7c5a4a191b216fecaa62e49
Alexander-Russakoff <tuo49330@temple.edu>
2023-03-04 21:22:50 -0500

    fix merge conflict

--- pocs-capstone/backend/db/serializers.py
+++ pocs-capstone/backend/db/serializers.py
@@ -1,24 +1,33 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from db.models import NewUser
 from django.contrib.auth.password_validation import validate_password
 
 
 
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
+class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['email', 'username', 'password']
+        model = NewUser
+        fields = ('email', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            username=validated_data['username']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        password = validated_data.pop('password',None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance 
     
+
+
+class AvatarSerializer(serializers.ModelSerializer):
+    pass
+
+class TaskSerializer(serializers.ModelSerializer):
+    pass
+
+class InventorySerializer(serializers.ModelSerializer):
+    pass
 
