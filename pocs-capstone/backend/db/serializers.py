6772062fc3a0512a94805fa4dfd23c5b5da34dff
Alexander-Russakoff <tuo49330@temple.edu>
2023-03-04 21:07:43 -0500

    serializer didn't make it to latest push?

--- pocs-capstone/backend/db/serializers.py
+++ pocs-capstone/backend/db/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from .models import Note
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Note
         fields = ('note_text', 'note_id')
+
+
+
+#Django Rest Framework fields can be configured to be "write only". The documentation even has an example that pretty much covers your usecase:
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
