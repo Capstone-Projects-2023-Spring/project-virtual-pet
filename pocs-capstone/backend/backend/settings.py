ddb6f3fe2a3779bb651677362dbf593fe0ebf945
Christine Cho <tun66463@temple.edu>
2023-03-07 18:14:19 -0500

    update database

--- pocs-capstone/backend/backend/settings.py
+++ pocs-capstone/backend/backend/settings.py
@@ -168,7 +168,7 @@ AUTH_USER_MODEL = "db.NewUser"
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS':False,
     'BLACKLIST_AFTER_ROTATION':True,
