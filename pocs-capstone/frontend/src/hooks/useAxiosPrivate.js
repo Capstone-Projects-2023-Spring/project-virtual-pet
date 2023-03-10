d3bbd352c40588cfc58ae0dfd52c3ea68eecefb6
Alexander-Russakoff <tuo49330@temple.edu>
2023-03-10 01:16:13 -0500

    prep rebasing from main

--- pocs-capstone/frontend/src/hooks/useAxiosPrivate.js
+++ pocs-capstone/frontend/src/hooks/useAxiosPrivate.js
@@ -12,7 +12,7 @@ const useAxiosPrivate = () => {
         const requestIntercept = axiosPrivate.interceptors.request.use(
             config => {
                 if (!config.headers['Authorization']) {
-                    config.headers['Authorization'] = `Bearer ${auth?.accessToken}`;
+                    config.headers['Authorization'] = `Bearer ${auth?.access}`;
                 }
                 return config;
             }, (error) => Promise.reject(error)
@@ -24,8 +24,8 @@ const useAxiosPrivate = () => {
                 const prevRequest = error?.config;
                 if (error?.response?.status === 403 && !prevRequest?.sent) {
                     prevRequest.sent = true;
-                    const newAccessToken = await refresh();
-                    prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                    const newAccess = await refresh();
+                    prevRequest.headers['Authorization'] = `Bearer ${newAccess}`;
                     return axiosPrivate(prevRequest);
                 }
                 return Promise.reject(error);
