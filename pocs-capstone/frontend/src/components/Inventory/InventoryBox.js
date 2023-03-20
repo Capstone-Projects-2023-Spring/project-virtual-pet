0ae73ec8c422e3a47fefd5273fb6795f80ffa41d
jay-newman <tup16252@temple.edu>
2023-03-19 22:37:35 -0400

    Only let user populate candy one time

--- pocs-capstone/frontend/src/components/Inventory/InventoryBox.js
+++ pocs-capstone/frontend/src/components/Inventory/InventoryBox.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { useEffect, useReducer} from "react";
+import { useEffect, useState} from "react";
 import Candy from "./Candy";
 import './Inventory.css'
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import { useContext } from "react";
 import InventoryContext from "../../context/InventoryContext";
 import PopulateInv from "./PopulateInv";
+import { boolean } from "yup";
 
 // Displays the inventory box and renders Candy components inside box based off inventory
 // Need to add css to css file 
@@ -20,41 +21,37 @@ function InventoryBox() {
         handlers?.getInventory()
         .then(inv =>{
             handlers.setInv(inv)
-            // console.log("Inventory fetched in Inventory Box")
-            // console.log(inv)
 
         })
     }
     
+    
     // Uncomment button to use function to help create candies
+    
     let postFullInventory = () => {
+        handlers?.getInventory().then(inv => {
+            if(inv.length === 0){
+                let fullInventoryData = PopulateInv()
 
-        let fullInventoryData = PopulateInv()
-
-        // Loop through and post full inventory to backend
-        for (let i = 0; i < 21; i++) {
-            // console.log(fullInventoryData[i])
-            let request = axiosPrivate.post(`${baseURL}`, (fullInventoryData[i]))
-            setTimeout(() => {
-            }, 1000);
-
-
-
-        }
-       
-        // let request = axiosPrivate.put(`${baseURL}${data.inventory_id}/`, (data))
-        // let request = axiosPrivate.post(`${baseURL}`, (data))
-
-        // return request.then(response => response.data)
-        // return request.then(response => response.data)
-
+                // Loop through and post full inventory to backend
+                for (let i = 0; i < 20; i++) {
+                    // console.log(fullInventoryData[i])
+                    let request = axiosPrivate.post(`${baseURL}`, (fullInventoryData[i]))
+                    setTimeout(() => {
+                    }, 1000);            
+                }
+                
+                handlers?.setInv(inv)
+            }
+        
+        })       
     }
 
     useEffect(fetchData, [])
     
     return (
             <>
-                <button onClick={postFullInventory}>Populate Candy</button>
+                <button onClick={postFullInventory}>I Want Candy!!!</button>
 
                 <h1 style={{
                     textAlign: "center",
@@ -80,8 +77,6 @@ function InventoryBox() {
                     
                         </div>
                 </div>
-                {/* <button onClick={refreshInventory}>Click Me</button> */}
-
             </>
     )
 }
