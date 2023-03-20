41ffefe2d5f1d2c21c5cded4f773fd7ceb568e67
Christine Cho <tun66463@temple.edu>
2023-03-20 16:00:52 -0400

    update createtaskform

--- pocs-capstone/frontend/src/components/PageDisplay/CreateTaskForm.js
+++ pocs-capstone/frontend/src/components/PageDisplay/CreateTaskForm.js
@@ -2,7 +2,7 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import TaskListContext from '../../context/TaskListContext'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import Stack from 'react-bootstrap/Stack';
 import * as yup from "yup";
 import * as formik from 'formik'
@@ -10,6 +10,11 @@ import * as formik from 'formik'
 
 function CreateTaskForm(props) {
   const handlers = useContext(TaskListContext);
+
+  const [showRecurr, setShowRecurr] = useState(false);
+
+  const handleCloseR = () => setShowRecurr(false);
+  const handleShowR = () => setShowRecurr(true);
 
   const readOnly = props.task ? true : false
   const title = props.task ? "Task Details" : "Create Task"
@@ -20,7 +25,10 @@ function CreateTaskForm(props) {
     title: yup.string().required(),
     description: yup.string().required(),
     size: yup.string().required(),
-    due_date: yup.string().required()
+    level: yup.string().required(),
+    due_date: yup.string().required(),
+    recurrence: yup.string().required(),
+
   });
 
   return (
@@ -46,7 +54,9 @@ function CreateTaskForm(props) {
             title: props.task ? props.task.title : '',
             description: props.task ? props.task.description : '',
             size: props.task ? props.task.task_type : 'S',
+            level: props.task ? props.task.task_level : 'Beginner',
             due_date: props.task ? props.task.due_date : '',
+            recurrence: props.task ? props.task.recurrence : 'never',
           }}
         >
           {({
@@ -85,9 +95,9 @@ function CreateTaskForm(props) {
                     name="description"
                     style={{ height: '100px' }}
                     value={values.description}
-                    onChange={handleChange} 
+                    onChange={handleChange}
                     isInvalid={!!errors.description} />
-                    <Form.Control.Feedback type="invalid">
+                  <Form.Control.Feedback type="invalid">
                     {errors.description}
                   </Form.Control.Feedback>
 
@@ -95,29 +105,65 @@ function CreateTaskForm(props) {
 
                 <br />
 
-                <Form.Group controlId="validationFormik03">
-                  <Form.Label>Task Size</Form.Label>
-                  <Form.Select
+                <Stack direction="horizontal" gap={3} className="mx-auto">
 
-                    placeholder="Select Size"
-                    name="size"
-                    value={values.size}
-                    onChange={handleChange}
-                    isInvalid={!!errors.size}
-                  >
-                    <option value="S">Small</option>
-                    <option value="M">Medium</option>
-                    <option value="L">Large</option>
-                    <option value="C">Cake</option>
-                  </Form.Select>
-                  <Form.Control.Feedback type="invalid">
-                    {errors.size}
-                  </Form.Control.Feedback>
-                </Form.Group>
+                  <Form.Group controlId="validationFormik03">
+                    <Form.Label>Task Size</Form.Label>
+                    <Form.Select
+
+                      placeholder="Select Size"
+                      name="size"
+                      value={values.size}
+                      onChange={handleChange}
+                      isInvalid={!!errors.size}
+                    >
+                      <option value="S">Small</option>
+                      <option value="M">Medium</option>
+                      <option value="L">Large</option>
+                      <option value="C">Cake</option>
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.size}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+
+                  <br />
+
+                  <Form.Group controlId="validationFormik04" className='ms-6'>
+                    <Form.Label>Task Level</Form.Label>
+                    <Form.Select
+
+                      placeholder="Select Level"
+                      name="level"
+                      value={values.size}
+                      onChange={handleChange}
+                      isInvalid={!!errors.level}
+                    >
+                      <option value="Beginner">Beginner</option>
+                      <option value="Novice"> Novice</option>
+                      <option value="Intermediate">Intermediate</option>
+                      <option value="Advanced">Advanced</option>
+                      <option value="Expert">Expert</option>
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                      {errors.level}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+
+
+                </Stack>
+
+
 
                 <br />
 
-                <Form.Group controlId="validationFormik04">
+
+
+
+
+
+
+                <Form.Group controlId="validationFormik05">
                   <Form.Label>Due Date</Form.Label>
                   <Form.Control
 
@@ -132,12 +178,57 @@ function CreateTaskForm(props) {
                   </Form.Control.Feedback>
                 </Form.Group>
 
+                <Form.Group controlId="validationFormik06">
+                  <Form.Label>Repeating? </Form.Label>
+                  <Form.Select
+
+                    placeholder="Select Level"
+                    name="recurrence"
+                    value={values.recurrence}
+                    onChange={(e) => {
+                      handleChange(e)
+                      if (e.target.value === "custom") {
+                        console.log(e.target.value, "HERE")
+                        handleShowR()
+                      }
+
+                    }}
+                    isInvalid={!!errors.level}
+                  >
+                    <option value="never">Never</option>
+                    <option value="custom"> Custom</option>
+                  </Form.Select>
+                  <Form.Control.Feedback type="invalid">
+                    {errors.recurrence}
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+
+
+
                 <Button className="col-md-5 mx-auto" type="submit">{buttonText}</Button>
 
               </Stack>
+
+
+              <Modal
+                show={showRecurr}
+                onHide={() => {
+                  handleCloseR()
+                  values.recurrence = 'never'
+                }}>
+                <Modal.Header closeButton>
+                  <Modal.Title>CUSTOM RECURRENCE</Modal.Title>
+                </Modal.Header>
+
+              </Modal>
+
             </Form>
           )}
+
         </Formik>
+
+
       </Modal.Body>
     </Modal >
 
