845700f5b81c4a317dbda87871f491db7517f659
mgclay <mary.clay@temple.edu>
2023-03-24 14:17:58 -0400

    added responsive spritesheet to main page

--- pocs-capstone/frontend/src/components/PetDisplay/PetSprite.js
+++ pocs-capstone/frontend/src/components/PetDisplay/PetSprite.js
@@ -1,8 +1,11 @@
 import kittycat from '../../images/orangecat.png';
-
+import Spritesheet from 'react-responsive-spritesheet'
 import bgimage from '../../images/bg.gif'
+import orangesheet from '../../../src/images/orange_happy_sheet.png'
+import graysheet from '../../../src/images/gray_happy_sheet.png' 
 import "./PetDisplay"
 import { useState } from 'react'
+import '../../../src/components/AnimateChoice.css'
 // import { useDrop } from "react-dnd";
 
 const PetSprite = ({ avatarInfo, setAvatar, inventory, setInventory }) => {
@@ -19,10 +22,12 @@ const PetSprite = ({ avatarInfo, setAvatar, inventory, setInventory }) => {
             case 'CT':
                 switch (pet.palette) {
                     case 0:
-                        return require('../../images/orangecat.png')
+                        //return require('../../images/orangecat.png')
+                        return orangesheet;
                     case 1:
                         // CHANGE TO IMAGE OF OTHER CAT (black palette)
-                        return require('../../images/graycat.png')
+                       // return require('../../images/graycat.png')
+                       return graysheet;
                 }
 
             case 'DG':
@@ -40,9 +45,24 @@ const PetSprite = ({ avatarInfo, setAvatar, inventory, setInventory }) => {
     return (
         <div className='p-sprite-display'>
             <img src={bgimage} alt="background" className="bg-sprite" />
-            <img src={avatarImage(avatarInfo)} alt="background" className="p-sprite" />
+
+            <Spritesheet
+                className = "p-sprite"
+                image={avatarImage(avatarInfo)}
+                stopLastFrame={true}
+                widthFrame={255}
+                heightFrame={350}
+                steps={5}
+                fps={3}
+                loop={false}
+                autoplay={false}
+                isResponsive={false}
+                onClick={spritesheet => {spritesheet.play()}}
+                />
         </div>
     )
 }
 
 export default PetSprite
+
+//<img src={avatarImage(avatarInfo)} alt="background" className="p-sprite" />
