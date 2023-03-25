ee035ff56b9e02dc382fc4204c46f0c7b755138d
Alexander-Russakoff <tuo49330@temple.edu>
2023-03-25 02:01:14 -0400

    format

--- pocs-capstone/frontend/src/components/AccountPage.js
+++ pocs-capstone/frontend/src/components/AccountPage.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Form, Row, Col, Button } from 'react-bootstrap';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import useAxiosPrivate from '../hooks/useAxiosPrivate'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 function AccountPage() {
   const axiosPrivate = useAxiosPrivate();
 
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [bio, setBio] = useState('');
-  const [joinDate, setjoinDate] = useState('');
-  const [birthday, setBirthday] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [joinDate, setjoinDate] = useState("");
+  const [birthday, setBirthday] = useState("");
 
   const [validated, setValidated] = useState(false);
 
   useEffect(() => {
-    axiosPrivate.get('/user-data/').then((response) => {
+    axiosPrivate.get("/user-data/").then((response) => {
       setUsername(response.data[0].username);
       setEmail(response.data[0].email);
       setName(response.data[0].first_name);
@@ -41,23 +41,30 @@ function AccountPage() {
 
   const update = () => {
     console.log("DATA TEST BEFORE: ", name, bio, birthday);
-    axiosPrivate.get('/user-data/').then((response) => {
-      const updatedUserInfo = {
-        first_name: name.trim() === '' ? response.data[0].first_name : name,
-        bio: bio.trim() === '' ? response.data[0].bio : bio,
-        birthday: birthday.trim() === '' ? response.data[0].birthday : birthday,
-      };
+    axiosPrivate
+      .get("/user-data/")
+      .then((response) => {
+        const updatedUserInfo = {
+          first_name: name.trim() === "" ? response.data[0].first_name : name,
+          bio: bio.trim() === "" ? response.data[0].bio : bio,
+          birthday:
+            birthday.trim() === "" ? response.data[0].birthday : birthday,
+        };
 
-      const id = response.data[0].id;
-      const url = '/user-data/' + id + "/";
-      axiosPrivate.patch(url, updatedUserInfo).then((response) => {
-        console.log("response.data:", response.data);
-      }).catch((err) => {
+        const id = response.data[0].id;
+        const url = "/user-data/" + id + "/";
+        axiosPrivate
+          .patch(url, updatedUserInfo)
+          .then((response) => {
+            console.log("response.data:", response.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
         console.log(err);
       });
-    }).catch((err) => {
-      console.log(err);
-    });
   };
 
   const handleSubmit = (event) => {
@@ -70,8 +77,8 @@ function AccountPage() {
       update();
     }
     setValidated(true);
-  }
-/*
+  };
+  /*
   const validateNameLength = (form) => {
     // Add custom validation for name field length
     const nameInput = form.elements["validationCustom01"];
@@ -118,9 +125,25 @@ function AccountPage() {
   */
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Form noValidate validated={validated} onSubmit={handleSubmit} style={{ maxWidth: '700px', width: '100%', padding: '20px', borderRadius: '5px', boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.2)' }}>
-
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        style={{
+          maxWidth: "700px",
+          width: "100%",
+          padding: "20px",
+          borderRadius: "5px",
+          boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.2)",
+        }}
+      >
         <div className="text-center mt-3 mb-4">
           <h1 className="h2 mb-0">Account Profile</h1>
         </div>
@@ -142,40 +165,66 @@ function AccountPage() {
         <Form.Group className="mb-2" controlId="validationCustom03">
           <Form.Label className="col-sm-2 col-form-label">Birthday:</Form.Label>
           <Col sm={4}>
-            <Form.Control required type="date" value={birthday} onChange={handleBirthdayChange} />
-            <Form.Control.Feedback type="invalid">Please provide your birthday.</Form.Control.Feedback>
+            <Form.Control
+              required
+              type="date"
+              value={birthday}
+              onChange={handleBirthdayChange}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide your birthday.
+            </Form.Control.Feedback>
           </Col>
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="validationCustom01">
           <Form.Label className="col-sm-2 col-form-label">Name:</Form.Label>
           <Col sm={4}>
-            <Form.Control required type="text" value={name} onChange={handleNameChange} />
-            <Form.Control.Feedback type="invalid">Please provide your name.</Form.Control.Feedback>
+            <Form.Control
+              required
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide your name.
+            </Form.Control.Feedback>
           </Col>
         </Form.Group>
 
         <Form.Group className="mb-2" controlId="validationCustom02">
           <Form.Label className="col-sm-2 col-form-label">Bio:</Form.Label>
           <Col sm={8}>
-            <Form.Control required as="textarea" rows={3} value={bio} onChange={handleBioChange} />
-            <Form.Control.Feedback type="invalid">Please provide a bio.</Form.Control.Feedback>
+            <Form.Control
+              required
+              as="textarea"
+              rows={3}
+              value={bio}
+              onChange={handleBioChange}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a bio.
+            </Form.Control.Feedback>
           </Col>
         </Form.Group>
 
         <Form.Group className="mb-2">
-          <Form.Label className="col-sm-2 col-form-label">Studying Since:</Form.Label>
+          <Form.Label className="col-sm-2 col-form-label">
+            Studying Since:
+          </Form.Label>
           <Col sm={4}>
             <Form.Control type="date" value={joinDate} disabled />
           </Col>
         </Form.Group>
 
-        <Button type="submit" style={{ marginTop: '20px', marginBottom: '20px' }}>Submit</Button>
-
+        <Button
+          type="submit"
+          style={{ marginTop: "20px", marginBottom: "20px" }}
+        >
+          Submit
+        </Button>
       </Form>
     </div>
-
-
   );
 }
 
--- pocs-capstone/frontend/src/components/PageDisplay/PetProfPage.js
+++ pocs-capstone/frontend/src/components/PageDisplay/PetProfPage.js
@@ -1,68 +1,104 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import { Form, Row, Col, Button } from 'react-bootstrap';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import useAxiosPrivate from '../../hooks/useAxiosPrivate'
-import AvatarContext from '../../context/AvatarContext';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import AvatarContext from "../../context/AvatarContext";
 
 function PetProfPage() {
-    const handlers = useContext(AvatarContext);
-    const axiosPrivate = useAxiosPrivate();
+  const handlers = useContext(AvatarContext);
+  const axiosPrivate = useAxiosPrivate();
 
-    const [petName, setPetName] = useState('');
-    const [flavourText, setFlavourText] = useState('');
+  const [petName, setPetName] = useState("");
+  const [flavourText, setFlavourText] = useState("");
 
-    const handlePetNameChange = (event) => {
-        const changedPetName = event.target.value;
-        setPetName(changedPetName);
+  const handlePetNameChange = (event) => {
+    const changedPetName = event.target.value;
+    setPetName(changedPetName);
+  };
+
+  const handleFlavourTextChange = (event) => {
+    const changedFlavourText = event.target.value;
+    setFlavourText(changedFlavourText);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("PET BEFORE UPDATE", handlers.avatarInfo);
+    const updatedAvatar = {
+      ...handlers?.avatarInfo,
+      pet_name: petName.trim() === "" ? handlers.avatarInfo.pet_name : petName,
+      flavour_text:
+        flavourText.trim() === ""
+          ? handlers.avatarInfo.flavour_text
+          : flavourText,
     };
+    axiosPrivate
+      .patch(`/avatar/${handlers?.avatarInfo.avatar_id}/`, updatedAvatar)
+      .then((response) => {
+        console.log("response.data:", response.data);
+        handlers?.setAvatar(response.data); //change this to add to previous state instead of replacing completely (in case of >1 avatar for 1 user)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const handleFlavourTextChange = (event) => {
-        const changedFlavourText = event.target.value;
-        setFlavourText(changedFlavourText);
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("PET BEFORE UPDATE", handlers.avatarInfo)
-       const updatedAvatar = {
-            ...handlers?.avatarInfo, 
-            pet_name: petName.trim() === '' ? handlers.avatarInfo.pet_name : petName, 
-            flavour_text: flavourText.trim() === '' ? handlers.avatarInfo.flavour_text : flavourText
-        };
-        axiosPrivate.patch(`/avatar/${handlers?.avatarInfo.avatar_id}/`, updatedAvatar).then((response) => {
-            console.log("response.data:", response.data);
-            handlers?.setAvatar(response.data); //change this to add to previous state instead of replacing completely (in case of >1 avatar for 1 user)
-        }).catch((err) => {
-            console.log(err);
-        });
-
-    };
-
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <Form onSubmit={handleSubmit} style={{ maxWidth: '700px', width: '100%', padding: '20px', borderRadius: '5px', boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.2)' }}>
-                <div className="text-center mt-3 mb-4">
-                    <h1 className="h2 mb-0">Pet Profile</h1>
-                </div>
-                <Form.Group className="mb-2">
-                    <Form.Label className="col-sm-2 col-form-label">Pet Name:</Form.Label>
-                    <Col sm={4}>
-                        <Form.Control type="text" value={petName} placeholder={handlers.avatarInfo.pet_name} onChange={handlePetNameChange} />
-                    </Col>
-                </Form.Group>
-                <Form.Group className="mb-2">
-                    <Form.Label className="col-sm-2 col-form-label">What's your pet up to?</Form.Label>
-                    <Col sm={8}>
-                        <Form.Control as="textarea" rows={3} value={flavourText} placeholder={handlers.avatarInfo.flavour_text} onChange={handleFlavourTextChange} />
-                    </Col>
-                </Form.Group>
-                <Button type="submit" style={{ marginTop: '20px', marginBottom: '20px' }}>Submit</Button>
-            </Form>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Form
+        onSubmit={handleSubmit}
+        style={{
+          maxWidth: "700px",
+          width: "100%",
+          padding: "20px",
+          borderRadius: "5px",
+          boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.2)",
+        }}
+      >
+        <div className="text-center mt-3 mb-4">
+          <h1 className="h2 mb-0">Pet Profile</h1>
         </div>
-
-
-    );
+        <Form.Group className="mb-2">
+          <Form.Label className="col-sm-2 col-form-label">Pet Name:</Form.Label>
+          <Col sm={4}>
+            <Form.Control
+              type="text"
+              value={petName}
+              placeholder={handlers.avatarInfo.pet_name}
+              onChange={handlePetNameChange}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label className="col-sm-2 col-form-label">
+            What's your pet up to?
+          </Form.Label>
+          <Col sm={8}>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={flavourText}
+              placeholder={handlers.avatarInfo.flavour_text}
+              onChange={handleFlavourTextChange}
+            />
+          </Col>
+        </Form.Group>
+        <Button
+          type="submit"
+          style={{ marginTop: "20px", marginBottom: "20px" }}
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default PetProfPage;
