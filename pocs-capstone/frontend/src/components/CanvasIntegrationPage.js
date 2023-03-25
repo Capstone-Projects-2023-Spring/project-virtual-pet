328f2ab452bb4f5dc0f6e49aa837712460cb7a49
Katrina Janeczko <tuk04893@temple.edu>
2023-03-25 00:43:20 -0400

    add loading image & correct spelling error

--- pocs-capstone/frontend/src/components/CanvasIntegrationPage.js
+++ pocs-capstone/frontend/src/components/CanvasIntegrationPage.js
@@ -6,9 +6,9 @@ import new_access_token from './new_access_token.png'
 import './CanvasIntegrationPage.css'
 import './AnimateChoice.css'
 import useAxiosPrivate from '../hooks/useAxiosPrivate'
-import {useNavigate }from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 //currently theres some repeat stuff in the two files canvas integration and animate choice.
-
+import cat from '../images/orangecat.png'
 
 //not right
 const CANVAS_URL = '/user-data/'
@@ -16,14 +16,14 @@ const COURSES_URL = '/canvas/'
 
 const CanvasIntegrationPage = () => {
     const axiosPrivate = useAxiosPrivate();
-  //  const [submittedText, setSubmittedText] = useState(null);
+    //  const [submittedText, setSubmittedText] = useState(null);
     const [canvas_token, setEnteredText] = useState("");
     const [nameError, setNameError] = useState('')
-    
-    
-    const [tokenReady,setTokenReady] = useState(false)
-    const [retrievingAssignments,setRetrievingAssignments] = useState(false) 
-    
+
+
+    const [tokenReady, setTokenReady] = useState(false)
+    const [retrievingAssignments, setRetrievingAssignments] = useState(false)
+
     const [submitText, setSubmitText] = useState("Submit")
 
     const nav = useNavigate()
@@ -35,7 +35,7 @@ const CanvasIntegrationPage = () => {
 
     };
 
-    const resetSubmitTokenState= (text) =>{
+    const resetSubmitTokenState = (text) => {
         setRetrievingAssignments(false)
         setSubmitText("Submit")
         setNameError(text)
@@ -46,7 +46,7 @@ const CanvasIntegrationPage = () => {
 
         if (tokenReady) {
 
-            console.log("TOKEN READY????---------->",tokenReady)
+            console.log("TOKEN READY????---------->", tokenReady)
 
 
             axiosPrivate.get(COURSES_URL).then((response) => {
@@ -55,13 +55,13 @@ const CanvasIntegrationPage = () => {
                 //return state and navigate to main
                 resetSubmitTokenState("")
                 nav('/')
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err)
                 resetSubmitTokenState("Error getting assignments. Please check your token and try again.")
             })
         }
 
-    }, [tokenReady,axiosPrivate,nav]) // there was an error here if axiosPrivate and nav were not present
+    }, [tokenReady, axiosPrivate, nav]) // there was an error here if axiosPrivate and nav were not present
 
 
 
@@ -69,7 +69,7 @@ const CanvasIntegrationPage = () => {
     const textChangeHandler = (i) => {
         setEnteredText(i.target.value);
         //console.log(i.target.value);
-      };
+    };
 
 
     const handleSubmit = (event) => {
@@ -77,27 +77,27 @@ const CanvasIntegrationPage = () => {
         //setRetrievingAssignments(true)
         //setSubmitText("LOADING...")
         //ready to start attempting to get assignmemnts
-        setSubmittingTokenState("Retrieing assignments. Please wait...")
-        
+        setSubmittingTokenState("Retrieving assignments. Please wait...")
+
         if (canvas_token !== "") {
             //setSubmittedText(canvas_token);
-            
-            axiosPrivate.get(CANVAS_URL).then((response) =>{
+
+            axiosPrivate.get(CANVAS_URL).then((response) => {
                 const id = response?.data[0].id
-                console.log("ID---->",response.data[0].id) 
-                
-                const url = CANVAS_URL+id+"/"
-                console.log("-----> ",url)
-                const data = JSON.stringify({canvas_token})
+                console.log("ID---->", response.data[0].id)
+
+                const url = CANVAS_URL + id + "/"
+                console.log("-----> ", url)
+                const data = JSON.stringify({ canvas_token })
                 axiosPrivate.put(url, data)
-                    .then((response )=>{
+                    .then((response) => {
                         console.log(response.data);
                         setTokenReady(true)
-                   
-                   
-                   
-                   
-                    }).catch((err)=>{
+
+
+
+
+                    }).catch((err) => {
                         console.log(err)
                         //setRetrievingAssignments(false)
                         //setSubmitText("Submit")
@@ -105,7 +105,7 @@ const CanvasIntegrationPage = () => {
 
 
                     })
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err)
                 resetSubmitTokenState("Error getting assignments. Please check your token and try again.")
 
@@ -114,7 +114,7 @@ const CanvasIntegrationPage = () => {
 
         }
         else {
-            console.log("NO NAME ENTERED")
+            console.log("NO TOKEN ENTERED")
             //setNameError('You must enter a token!')
             //setSubmitText("Submit")
             //setRetrievingAssignments(false)
@@ -122,34 +122,54 @@ const CanvasIntegrationPage = () => {
 
         }
     }
-    return (
-        <div className = "body">
-            <Card style= {{width: '130rem'}}>
-            <Card.Header className = 'pet-choice'><center><h1>ADD YOUR CANVAS ACCOUNT!</h1></center></Card.Header> </Card>
-            <hr />
-             <Card.Header className = 'header'>So you want your pet to help you track your Canvas assignments...  </Card.Header>
-             <Card.Title className = 'text'>It's easy! Just follow these steps: </Card.Title>
-             <Card.Body className='text'>
-             <p> 1. Access your Canvas account, and select your profile on the sidebar:  </p>
-             <p></p><img src = {canvas_sidebar} alt = "Canvas Sidebar"></img>
-             <p></p> 2. Select settings. 
-             <p></p> 3. Under approved integrations, scroll all the way to the bottom! 
-             <p></p> Select "New access token". 
-             <p></p> <img src = {new_access_token} alt = "+ New access token"></img>
-             <p></p> 4. Enter a purpose and expiration date (ex. study buddy, and the end of your semester date). 
-             <p></p> Select "Generate token", and copy and paste it here! </Card.Body>
+  
+    const loadingImage = (
+        <div className="loading-container">
+      <img className="loading-image"
+        src={cat} alt="Loading..."
+      />
+      </div>
+    );
 
-             <form className = "submit_canvas" onSubmit={(event) => handleSubmit(event)}>
-                    <input className = "input" type = "text" placeholder= "Enter token here!" value={canvas_token} disabled={retrievingAssignments ? true:false} onChange={textChangeHandler}/>
-                    <button className="button" type="submit" disabled={retrievingAssignments ? true:false}>
-                        {submitText}
-                    </button>
-                    {nameError!=="" ? <p>{nameError}</p> : <></>}
+    return (
+        <div className="body">
+            {retrievingAssignments ? (
+                <div className="loading-parent">
+                    {loadingImage}
+                </div>
+            ) : (
+
+                <>
+                    <Card style={{ width: '130rem' }}>
+                        <Card.Header className='pet-choice'><center><h1>ADD YOUR CANVAS ACCOUNT!</h1></center></Card.Header> </Card>
+                    <hr />
+                    <Card.Header className='header'>So you want your pet to help you track your Canvas assignments...  </Card.Header>
+                    <Card.Title className='text'>It's easy! Just follow these steps: </Card.Title>
+                    <Card.Body className='text'>
+                        <p> 1. Access your Canvas account, and select your profile on the sidebar:  </p>
+                        <p></p><img src={canvas_sidebar} alt="Canvas Sidebar"></img>
+                        <p></p> 2. Select settings.
+                        <p></p> 3. Under approved integrations, scroll all the way to the bottom!
+                        <p></p> Select "New access token".
+                        <p></p> <img src={new_access_token} alt="+ New access token"></img>
+                        <p></p> 4. Enter a purpose and expiration date (ex. study buddy, and the end of your semester date).
+                        <p></p> Select "Generate token", and copy and paste it here! </Card.Body>
+
+
+
+                </>
+            )}
+
+            <form className="submit_canvas" onSubmit={(event) => handleSubmit(event)}>
+                <input className="input" type="text" placeholder="Enter token here!" value={canvas_token} disabled={retrievingAssignments ? true : false} onChange={textChangeHandler} />
+                <button className="button" type="submit" disabled={retrievingAssignments ? true : false}>
+                    {submitText}
+                </button>
+                {nameError !== "" ? <p>{nameError}</p> : <></>}
             </form>
 
-            
         </div>
-    )
-}
+    );
 
+} 
 export default CanvasIntegrationPage
