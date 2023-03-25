aac9145b8d98f11e6d617734555e207c161417e8
Katrina Janeczko <tuk04893@temple.edu>
2023-03-24 23:05:28 -0400

    add form validation --> user must enter name, birthday, bio to submit

--- pocs-capstone/frontend/src/components/AccountPage.js
+++ pocs-capstone/frontend/src/components/AccountPage.js
@@ -14,6 +14,8 @@ function AccountPage() {
   const [joinDate, setjoinDate] = useState('');
   const [birthday, setBirthday] = useState('');
 
+  const [validated, setValidated] = useState(false);
+
   useEffect(() => {
     axiosPrivate.get('/user-data/').then((response) => {
       setUsername(response.data[0].username);
@@ -37,8 +39,7 @@ function AccountPage() {
     setBirthday(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const update = () => {
     console.log("DATA TEST BEFORE: ", name, bio, birthday);
     axiosPrivate.get('/user-data/').then((response) => {
       const updatedUserInfo = {
@@ -58,52 +59,119 @@ function AccountPage() {
       console.log(err);
     });
   };
-      
 
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      event.preventDefault();
+      update();
+    }
+    setValidated(true);
+  }
+/*
+  const validateNameLength = (form) => {
+    // Add custom validation for name field length
+    const nameInput = form.elements["validationCustom01"];
+    const nameValue = nameInput.value;
+    if (nameValue.length > 32) {
+      nameInput.setCustomValidity("Name cannot be longer than 32 characters.");
+      nameInput.reportValidity();
+      return false;
+    } else {
+      nameInput.setCustomValidity("");
+      return true;
+    }
+  }
+
+  const validateBioLength = (form) => {
+    // Add custom validation for bio field length
+    const nameInput = form.elements["validationCustom02"];
+    const nameValue = nameInput.value;
+    if (nameValue.length > 32) {
+      nameInput.setCustomValidity("Bio cannot be longer than 512 characters.");
+      nameInput.reportValidity();
+      return false;
+    } else {
+      nameInput.setCustomValidity("");
+      return true;
+    }
+  }
+
+  const validateBirthday = (form) => {
+    // Add custom validation for birthday field
+    const dateInput = form.elements["validationCustom03"];
+    const dateValue = new Date(dateInput.value);
+    const ago = new Date();
+    ago.setFullYear(ago.getFullYear() - 5);
+    if (dateValue > ago) {
+      dateInput.setCustomValidity("Awfully suspicious that you are under 5 years old...");
+      dateInput.reportValidity();
+      return false;
+    } else {
+      dateInput.setCustomValidity("");
+      return true;
+    }
+  }
+  */
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Form onSubmit={handleSubmit} style={{ maxWidth: '700px', width: '100%', padding: '20px', borderRadius: '5px', boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.2)' }}>
+      <Form noValidate validated={validated} onSubmit={handleSubmit} style={{ maxWidth: '700px', width: '100%', padding: '20px', borderRadius: '5px', boxShadow: '0px 0px 5px 0px rgba(0,0,0,0.2)' }}>
+
         <div className="text-center mt-3 mb-4">
           <h1 className="h2 mb-0">Account Profile</h1>
         </div>
+
         <Form.Group className="mb-2">
           <Form.Label className="col-sm-2 col-form-label">Username:</Form.Label>
           <Col sm={4}>
             <Form.Control type="text" value={username} disabled />
           </Col>
         </Form.Group>
+
         <Form.Group className="mb-2">
           <Form.Label className="col-sm-2 col-form-label">Email:</Form.Label>
           <Col sm={4}>
             <Form.Control type="email" value={email} disabled />
           </Col>
         </Form.Group>
-        <Form.Group className="mb-2">
+
+        <Form.Group className="mb-2" controlId="validationCustom03">
           <Form.Label className="col-sm-2 col-form-label">Birthday:</Form.Label>
           <Col sm={4}>
-            <Form.Control type="date" value={birthday} onChange={handleBirthdayChange} />
+            <Form.Control required type="date" value={birthday} onChange={handleBirthdayChange} />
+            <Form.Control.Feedback type="invalid">Please provide your birthday.</Form.Control.Feedback>
           </Col>
         </Form.Group>
-        <Form.Group className="mb-2">
+
+        <Form.Group className="mb-2" controlId="validationCustom01">
           <Form.Label className="col-sm-2 col-form-label">Name:</Form.Label>
           <Col sm={4}>
-            <Form.Control type="text" value={name} onChange={handleNameChange} />
+            <Form.Control required type="text" value={name} onChange={handleNameChange} />
+            <Form.Control.Feedback type="invalid">Please provide your name.</Form.Control.Feedback>
           </Col>
         </Form.Group>
-        <Form.Group className="mb-2">
+
+        <Form.Group className="mb-2" controlId="validationCustom02">
           <Form.Label className="col-sm-2 col-form-label">Bio:</Form.Label>
           <Col sm={8}>
-            <Form.Control as="textarea" rows={3} value={bio} onChange={handleBioChange} />
+            <Form.Control required as="textarea" rows={3} value={bio} onChange={handleBioChange} />
+            <Form.Control.Feedback type="invalid">Please provide a bio.</Form.Control.Feedback>
           </Col>
         </Form.Group>
+
         <Form.Group className="mb-2">
           <Form.Label className="col-sm-2 col-form-label">Studying Since:</Form.Label>
           <Col sm={4}>
             <Form.Control type="date" value={joinDate} disabled />
           </Col>
         </Form.Group>
+
         <Button type="submit" style={{ marginTop: '20px', marginBottom: '20px' }}>Submit</Button>
+
       </Form>
     </div>
 
