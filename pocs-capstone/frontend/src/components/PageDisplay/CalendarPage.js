ea653d1ad8e3d85edce80e75b81eea40d8cf1995
Katrina Janeczko <tuk04893@temple.edu>
2023-03-25 21:00:46 -0400

    messing around with calendar

--- pocs-capstone/frontend/src/components/PageDisplay/CalendarPage.js
+++ pocs-capstone/frontend/src/components/PageDisplay/CalendarPage.js
@@ -1,8 +1,32 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import TaskItem from "./TaskItem";
+
 const CalendarPage = () => {
-    return(
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+        setShowPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
+    return (
         <div className="calendar-page">
+            <Calendar value={selectedDate} onChange={handleDateChange} />
+            {showPopup && (
+                <div className="popup">
+                    <TaskItem selectedDate={selectedDate} onClose={handleClosePopup} />
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default CalendarPage
+
+
