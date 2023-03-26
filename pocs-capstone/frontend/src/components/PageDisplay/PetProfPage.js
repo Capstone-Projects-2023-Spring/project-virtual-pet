18d23d2f99e916ccc2f82a82b0e2d83f40535898
Katrina Janeczko <tuk04893@temple.edu>
2023-03-25 21:13:33 -0400

    made petprofpage css/styling more consistent with rest of main page

--- pocs-capstone/frontend/src/components/PageDisplay/PetProfPage.js
+++ pocs-capstone/frontend/src/components/PageDisplay/PetProfPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Col, Button, Card, Stack} from "react-bootstrap";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import AvatarContext from "../../context/AvatarContext";
@@ -45,60 +45,60 @@ function PetProfPage() {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Form
-        onSubmit={handleSubmit}
-        style={{
-          maxWidth: "700px",
-          width: "100%",
-          padding: "20px",
-          borderRadius: "5px",
-          boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.2)",
-        }}
-      >
-        <div className="text-center mt-3 mb-4">
-          <h1 className="h2 mb-0">Pet Profile</h1>
-        </div>
-        <Form.Group className="mb-2">
-          <Form.Label className="col-sm-2 col-form-label">Pet Name:</Form.Label>
-          <Col sm={4}>
-            <Form.Control
-              type="text"
-              value={petName}
-              placeholder={handlers.avatarInfo.pet_name}
-              onChange={handlePetNameChange}
-            />
-          </Col>
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label className="col-sm-2 col-form-label">
-            What's your pet up to?
-          </Form.Label>
-          <Col sm={8}>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              value={flavourText}
-              placeholder={handlers.avatarInfo.flavour_text}
-              onChange={handleFlavourTextChange}
-            />
-          </Col>
-        </Form.Group>
-        <Button
-          type="submit"
-          style={{ marginTop: "20px", marginBottom: "20px" }}
-        >
-          Submit
-        </Button>
-      </Form>
+
+    <div className="mini-page">
+
+      <Card className='pet-profile-position'>
+        <Card.Header>
+          <Stack direction="horizontal" gap={3}>
+            <div className='pet-profile-header'>Pet Profile</div>
+          </Stack>
+        </Card.Header>
+
+          <Form
+            onSubmit={handleSubmit}
+            style={{ padding: "20px",}}
+            variant="flush"
+          >
+
+            <Form.Group className="mb-2">
+              <Form.Label>Pet Name:</Form.Label>
+              <Col sm={4}>
+                <Form.Control
+                  type="text"
+                  value={petName}
+                  placeholder={handlers.avatarInfo.pet_name}
+                  onChange={handlePetNameChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group className="mb-2">
+              <Form.Label> What's your pet up to? </Form.Label>
+              <Col sm={8}>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={flavourText}
+                  placeholder={handlers.avatarInfo.flavour_text}
+                  onChange={handleFlavourTextChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Button
+              type="submit"
+              style={{ marginTop: "20px", marginBottom: "20px" }}
+            >
+              Submit
+            </Button>
+
+          </Form>
+        
+      </Card>
     </div>
   );
 }
 
 export default PetProfPage;
+
