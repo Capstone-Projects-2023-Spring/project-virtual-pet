50eb1cfbcc44e4014f2169940d02bf0ea919d095
Katrina Janeczko <tuk04893@temple.edu>
2023-03-25 21:02:58 -0400

    added css for account page that's consistent with the home page (main)

--- pocs-capstone/frontend/src/components/AccountPage.js
+++ pocs-capstone/frontend/src/components/AccountPage.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import { Form, Row, Col, Button, Card, Stack } from "react-bootstrap";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import './AccountPage.css'
 
 function AccountPage() {
   const axiosPrivate = useAxiosPrivate();
@@ -125,105 +126,96 @@ function AccountPage() {
   */
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Form
-        noValidate
-        validated={validated}
-        onSubmit={handleSubmit}
-        style={{
-          maxWidth: "700px",
-          width: "100%",
-          padding: "20px",
-          borderRadius: "5px",
-          boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.2)",
-        }}
-      >
-        <div className="text-center mt-3 mb-4">
-          <h1 className="h2 mb-0">Account Profile</h1>
-        </div>
-
-        <Form.Group className="mb-2">
-          <Form.Label className="col-sm-2 col-form-label">Username:</Form.Label>
-          <Col sm={4}>
-            <Form.Control type="text" value={username} disabled />
-          </Col>
-        </Form.Group>
-
-        <Form.Group className="mb-2">
-          <Form.Label className="col-sm-2 col-form-label">Email:</Form.Label>
-          <Col sm={4}>
-            <Form.Control type="email" value={email} disabled />
-          </Col>
-        </Form.Group>
-
-        <Form.Group className="mb-2" controlId="validationCustom03">
-          <Form.Label className="col-sm-2 col-form-label">Birthday:</Form.Label>
-          <Col sm={4}>
-            <Form.Control
-              required
-              type="date"
-              value={birthday}
-              onChange={handleBirthdayChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide your birthday.
-            </Form.Control.Feedback>
-          </Col>
-        </Form.Group>
-
-        <Form.Group className="mb-2" controlId="validationCustom01">
-          <Form.Label className="col-sm-2 col-form-label">Name:</Form.Label>
-          <Col sm={4}>
-            <Form.Control
-              required
-              type="text"
-              value={name}
-              onChange={handleNameChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide your name.
-            </Form.Control.Feedback>
-          </Col>
-        </Form.Group>
-
-        <Form.Group className="mb-2" controlId="validationCustom02">
-          <Form.Label className="col-sm-2 col-form-label">Bio:</Form.Label>
-          <Col sm={8}>
-            <Form.Control
-              required
-              as="textarea"
-              rows={3}
-              value={bio}
-              onChange={handleBioChange}
-            />
-            <Form.Control.Feedback type="invalid">
-              Please provide a bio.
-            </Form.Control.Feedback>
-          </Col>
-        </Form.Group>
-
-        <Form.Group className="mb-2">
-          <Form.Label className="col-sm-2 col-form-label">
-            Studying Since:
-          </Form.Label>
-          <Col sm={4}>
-            <Form.Control type="date" value={joinDate} disabled />
-          </Col>
-        </Form.Group>
-
-        <Button
-          type="submit"
-          style={{ marginTop: "20px", marginBottom: "20px" }}
+    <div className="account-page-display">
+      <Card className='account-profile-card'>
+        <Card.Header>
+          <div className='account-profile-header'>Account Profile</div>
+        </Card.Header>
+        <Form
+          className="form-content"
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
         >
-          Submit
-        </Button>
-      </Form>
+
+          <Form.Group className="mb-2">
+            <Form.Label>Username:</Form.Label>
+            <Col sm={4}>
+              <Form.Control type="text" value={username} disabled />
+            </Col>
+          </Form.Group>
+
+          <Form.Group className="mb-2">
+            <Form.Label>Email:</Form.Label>
+            <Col sm={4}>
+              <Form.Control type="email" value={email} disabled />
+            </Col>
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="validationCustom03">
+            <Form.Label>Birthday:</Form.Label>
+            <Col sm={4}>
+              <Form.Control
+                required
+                type="date"
+                value={birthday}
+                onChange={handleBirthdayChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide your birthday.
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="validationCustom01">
+            <Form.Label>Name:</Form.Label>
+            <Col sm={4}>
+              <Form.Control
+                required
+                type="text"
+                value={name}
+                onChange={handleNameChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide your name.
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="validationCustom02">
+            <Form.Label>Bio:</Form.Label>
+            <Col sm={8}>
+              <Form.Control
+                required
+                as="textarea"
+                rows={3}
+                value={bio}
+                onChange={handleBioChange}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a bio.
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Group>
+
+          <Form.Group className="mb-2">
+            <Form.Label>
+              Studying Since:
+            </Form.Label>
+            <Col sm={4}>
+              <Form.Control type="date" value={joinDate} disabled />
+            </Col>
+          </Form.Group>
+
+          <div className="submit-button">
+            <Button
+              style={{ marginTop: '20px', marginBottom: '20px'}}
+              type="submit">Submit</Button>
+          </div>
+
+        </Form>
+
+      </Card>
     </div>
   );
 }
