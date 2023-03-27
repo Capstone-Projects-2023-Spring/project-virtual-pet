4f1a72c7d051464d19f75fb5bfdbbc6d2d96a521
Katrina Janeczko <tuk04893@temple.edu>
2023-03-27 11:59:14 -0400

    Revert "messing around with calendar"
    This reverts commit ea653d1ad8e3d85edce80e75b81eea40d8cf1995.

--- pocs-capstone/frontend/src/components/PageDisplay/CalendarPage.js
+++ pocs-capstone/frontend/src/components/PageDisplay/CalendarPage.js
@@ -1,32 +1,8 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import TaskItem from "./TaskItem";
-
 const CalendarPage = () => {
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [showPopup, setShowPopup] = useState(false);
-
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
-        setShowPopup(true);
-    };
-
-    const handleClosePopup = () => {
-        setShowPopup(false);
-    };
-
-    return (
+    return(
         <div className="calendar-page">
-            <Calendar value={selectedDate} onChange={handleDateChange} />
-            {showPopup && (
-                <div className="popup">
-                    <TaskItem selectedDate={selectedDate} onClose={handleClosePopup} />
-                </div>
-            )}
         </div>
-    );
-};
+    )
+}
 
 export default CalendarPage
-
-
