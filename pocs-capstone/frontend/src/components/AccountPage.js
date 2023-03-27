5804c5c1c336e28726c6564b821f91dde203eda3
Alexander-Russakoff <tuo49330@temple.edu>
2023-03-27 17:15:22 -0400

    k

--- pocs-capstone/frontend/src/components/AccountPage.js
+++ pocs-capstone/frontend/src/components/AccountPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Form, Col, Button, Card } from "react-bootstrap";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import './AccountPage.css'
+import "./AccountPage.css";
 
 function AccountPage() {
   const axiosPrivate = useAxiosPrivate();
@@ -80,96 +80,94 @@ function AccountPage() {
 
   return (
     <div className="account-page-display">
-      <Card className='account-profile-card'>
+      <Card className="account-profile-card">
         <Card.Header>
-          <div className='account-profile-header'>Account Profile</div>
+          <div className="account-profile-header">Account Profile</div>
         </Card.Header>
         <div className="mx-auto">
-        <Form
-          className="form-content"
-          noValidate
-          validated={validated}
-          onSubmit={handleSubmit}
-        >
+          <Form
+            className="form-content"
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+          >
+            <Form.Group className="mb-2">
+              <Form.Label>Username:</Form.Label>
+              <Col>
+                <Form.Control type="text" value={username} disabled />
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>Username:</Form.Label>
-            <Col>
-              <Form.Control type="text" value={username} disabled />
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Email:</Form.Label>
+              <Col>
+                <Form.Control type="email" value={email} disabled />
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>Email:</Form.Label>
-            <Col>
-              <Form.Control type="email" value={email} disabled />
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2" controlId="validationCustom03">
+              <Form.Label>Birthday:</Form.Label>
+              <Col>
+                <Form.Control
+                  required
+                  type="date"
+                  value={birthday}
+                  onChange={handleBirthdayChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide your birthday.
+                </Form.Control.Feedback>
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2" controlId="validationCustom03">
-            <Form.Label>Birthday:</Form.Label>
-            <Col>
-              <Form.Control
-                required
-                type="date"
-                value={birthday}
-                onChange={handleBirthdayChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide your birthday.
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2" controlId="validationCustom01">
+              <Form.Label>Name:</Form.Label>
+              <Col>
+                <Form.Control
+                  required
+                  type="text"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide your name.
+                </Form.Control.Feedback>
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2" controlId="validationCustom01">
-            <Form.Label>Name:</Form.Label>
-            <Col>
-              <Form.Control
-                required
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide your name.
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2" controlId="validationCustom02">
+              <Form.Label>Bio:</Form.Label>
+              <Col>
+                <Form.Control
+                  required
+                  as="textarea"
+                  rows={3}
+                  value={bio}
+                  onChange={handleBioChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a bio.
+                </Form.Control.Feedback>
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2" controlId="validationCustom02">
-            <Form.Label>Bio:</Form.Label>
-            <Col>
-              <Form.Control
-                required
-                as="textarea"
-                rows={3}
-                value={bio}
-                onChange={handleBioChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a bio.
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Studying Since:</Form.Label>
+              <Col>
+                <Form.Control type="date" value={joinDate} disabled />
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>
-              Studying Since:
-            </Form.Label>
-            <Col>
-              <Form.Control type="date" value={joinDate} disabled />
-            </Col>
-          </Form.Group>
-
-          <div className="submit-button">
-            <Button
-              style={{ marginTop: '20px', marginBottom: '20px'}}
-              type="submit">Submit</Button>
-          </div>
-
-        </Form>
+            <div className="submit-button">
+              <Button
+                style={{ marginTop: "20px", marginBottom: "20px" }}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </div>
+          </Form>
         </div>
-
       </Card>
     </div>
   );
