0473501961890da2d1141e4b6b4d7bf7b4b62ee8
Christine Cho <tun66463@temple.edu>
2023-03-28 00:00:32 -0400

    filter inventories, add grayscale when empty, refactor, etc

--- pocs-capstone/frontend/src/components/Inventory/Candy.js
+++ pocs-capstone/frontend/src/components/Inventory/Candy.js
@@ -3,9 +3,9 @@ import { useDrag } from "react-dnd";
 import './Inventory.css'
 import { Badge, OverlayTrigger, Tooltip, Card } from 'react-bootstrap';
 // Renders Candy component based off props passed from InventoryBox's inventory state
-function Candy ({id, quantity, candy_base_type, candy_level}) {
-    
-    let [{isDragging}, drag] = useDrag(() => ({
+function Candy({ id, quantity, candy_base_type, candy_level }) {
+
+    let [{ isDragging }, drag] = useDrag(() => ({
         type: "image",
         item: { id: id },
         // Optional collect function used for accessing isDragging boolean
@@ -15,90 +15,86 @@ function Candy ({id, quantity, candy_base_type, candy_level}) {
 
     }));
 
-       // Determine candy image to render
+    // Determine candy image to render
     let candyImage = () => {
-    switch (candy_base_type) {
-        case 'S':  
-            switch (candy_level) {
-                case 1:
-                    return require('../../images/candies/candies_resized/small_1_scaled_4x_pngcrushed.png')
-                case 2: 
-                    return require('../../images/candies/candies_resized/small_2_scaled_4x_pngcrushed.png')
-                case 3:  
-                    return require('../../images/candies/candies_resized/small_3_scaled_4x_pngcrushed.png')
-                case 4:
-                    return require('../../images/candies/candies_resized/small_4_scaled_4x_pngcrushed.png')
-                case 5: 
-                    return require('../../images/candies/candies_resized/small_5_scaled_4x_pngcrushed.png')
-                // Default to level one
-                default:
-                    return require(`../../images/candies/candies_resized/small_1_scaled_4x_pngcrushed.png`) 
-            }
-        case 'M':
-            switch (candy_level) {
-                case 1:
-                    return require('../../images/candies/candies_resized/med_1_scaled_5x_pngcrushed.png')
-                case 2: 
-                    return require('../../images/candies/candies_resized/med_2_scaled_5x_pngcrushed.png')
-                case 3:  
-                    return require('../../images/candies/candies_resized/med_3_scaled_5x_pngcrushed.png')
-                case 4:
-                    return require('../../images/candies/candies_resized/med_4_scaled_5x_pngcrushed.png')
-                case 5:  
-                   return require('../../images/candies/candies_resized/med_5_scaled_5x_pngcrushed.png')
-                // Default to level one
-                default:
-                    return require(`../../images/candies/candies_resized/med_1_scaled_5x_pngcrushed.png`)
-            }
-        case 'L':
-            switch (candy_level) {
-                case 1:
-                    return require(`../../images/candies/candies_resized/large_1_scaled_4x_pngcrushed.png`)
-                case 2: 
-                    return require(`../../images/candies/candies_resized/large_2_scaled_4x_pngcrushed.png`)
-                case 3:  
-                    return require(`../../images/candies/candies_resized/large_3_scaled_4x_pngcrushed.png`)
-                case 4:
-                    return require(`../../images/candies/candies_resized/large_4_scaled_4x_pngcrushed.png`)
-                case 5:   
-                    return require(`../../images/candies/candies_resized/large_5_scaled_4x_pngcrushed.png`)
-                // Default to level one
-                default:
-                    return require(`../../images/candies/candies_resized/large_1_scaled_4x_pngcrushed.png`)
-            }
-        case 'C':
-            switch (candy_level) {
-                case 1:
-                    return require(`../../images/candies/candies_resized/cake_1_scaled_5x_pngcrushed.png`)
-                case 2: 
-                    return require(`../../images/candies/candies_resized/cake_2_scaled_5x_pngcrushed.png`)
-                case 3:  
-                    return require(`../../images/candies/candies_resized/cake_3_scaled_5x_pngcrushed.png`)
-                case 4:
-                    return require(`../../images/candies/candies_resized/cake_4_scaled_5x_pngcrushed.png`)
-                case 5:   
-                    return require(`../../images/candies/candies_resized/cake_5_scaled_5x_pngcrushed.png`)
-                // Default to level one
-                default:
-                    return require(`../../images/candies/candies_resized/cake_1_scaled_5x_pngcrushed.png`)
-            }
+        switch (candy_base_type) {
+            case 'S':
+                switch (candy_level) {
+                    case 1:
+                        return require('../../images/candies/candies_resized/small_1_scaled_4x_pngcrushed.png')
+                    case 2:
+                        return require('../../images/candies/candies_resized/small_2_scaled_4x_pngcrushed.png')
+                    case 3:
+                        return require('../../images/candies/candies_resized/small_3_scaled_4x_pngcrushed.png')
+                    case 4:
+                        return require('../../images/candies/candies_resized/small_4_scaled_4x_pngcrushed.png')
+                    case 5:
+                        return require('../../images/candies/candies_resized/small_5_scaled_4x_pngcrushed.png')
+                    // Default to level one
+                    default:
+                        return require(`../../images/candies/candies_resized/small_1_scaled_4x_pngcrushed.png`)
+                }
+            case 'M':
+                switch (candy_level) {
+                    case 1:
+                        return require('../../images/candies/candies_resized/med_1_scaled_5x_pngcrushed.png')
+                    case 2:
+                        return require('../../images/candies/candies_resized/med_2_scaled_5x_pngcrushed.png')
+                    case 3:
+                        return require('../../images/candies/candies_resized/med_3_scaled_5x_pngcrushed.png')
+                    case 4:
+                        return require('../../images/candies/candies_resized/med_4_scaled_5x_pngcrushed.png')
+                    case 5:
+                        return require('../../images/candies/candies_resized/med_5_scaled_5x_pngcrushed.png')
+                    // Default to level one
+                    default:
+                        return require(`../../images/candies/candies_resized/med_1_scaled_5x_pngcrushed.png`)
+                }
+            case 'L':
+                switch (candy_level) {
+                    case 1:
+                        return require(`../../images/candies/candies_resized/large_1_scaled_4x_pngcrushed.png`)
+                    case 2:
+                        return require(`../../images/candies/candies_resized/large_2_scaled_4x_pngcrushed.png`)
+                    case 3:
+                        return require(`../../images/candies/candies_resized/large_3_scaled_4x_pngcrushed.png`)
+                    case 4:
+                        return require(`../../images/candies/candies_resized/large_4_scaled_4x_pngcrushed.png`)
+                    case 5:
+                        return require(`../../images/candies/candies_resized/large_5_scaled_4x_pngcrushed.png`)
+                    // Default to level one
+                    default:
+                        return require(`../../images/candies/candies_resized/large_1_scaled_4x_pngcrushed.png`)
+                }
+            case 'C':
+                switch (candy_level) {
+                    case 1:
+                        return require(`../../images/candies/candies_resized/cake_1_scaled_5x_pngcrushed.png`)
+                    case 2:
+                        return require(`../../images/candies/candies_resized/cake_2_scaled_5x_pngcrushed.png`)
+                    case 3:
+                        return require(`../../images/candies/candies_resized/cake_3_scaled_5x_pngcrushed.png`)
+                    case 4:
+                        return require(`../../images/candies/candies_resized/cake_4_scaled_5x_pngcrushed.png`)
+                    case 5:
+                        return require(`../../images/candies/candies_resized/cake_5_scaled_5x_pngcrushed.png`)
+                    // Default to level one
+                    default:
+                        return require(`../../images/candies/candies_resized/cake_1_scaled_5x_pngcrushed.png`)
+                }
             // Default Candy
             default:
                 return require('../../images/candies/candies_resized/cake_1_scaled_5x_pngcrushed.png')
         }
     }
 
-    const getStyle = () => {
-        return { backgroundColor: isDragging ? 'white' : 'red' }
-    }
-
     return (
-        <div >
+        <>
 
             {/* OverlayTrigger - when a user hovers over the candy item a little Tooltip component w/ the candy's details will pop up */}
             < OverlayTrigger placement='bottom' overlay={
                 < Tooltip className="tooltip-item" >
-                    <span>Candy Base type: <strong>{candy_base_type}</strong> </span>
+                    <span>Candy Base Type: <strong>{candy_base_type}</strong> </span>
                     <span>Candy Level: <strong>{candy_level}</strong></span>
                 </Tooltip >
             }
@@ -111,8 +107,8 @@ function Candy ({id, quantity, candy_base_type, candy_level}) {
                         <div className="candy-wrapper" ref={drag}>
                             <img className="candy-photo"
                                 src={candyImage(candy_base_type)}
-                                alt="Candy" />
-
+                                alt="Candy"
+                                style={{ filter: quantity===0 ?  "grayscale(100%)" : '' }} />
                         </div>
 
                     </div>
@@ -120,7 +116,7 @@ function Candy ({id, quantity, candy_base_type, candy_level}) {
 
             </OverlayTrigger >
 
-        </div >
+        </>
 
     )
     // }
--- pocs-capstone/frontend/src/components/Inventory/InventoryBox.js
+++ pocs-capstone/frontend/src/components/Inventory/InventoryBox.js
@@ -6,7 +6,7 @@ import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import { useContext } from "react";
 import InventoryContext from "../../context/InventoryContext";
 import PopulateInv from "./PopulateInv";
-import { Card, Stack, Button } from 'react-bootstrap';
+import { Card, Stack, Button, ListGroup, Badge } from 'react-bootstrap';
 
 
 // Displays the inventory box and renders Candy components inside box based off inventory
@@ -15,13 +15,29 @@ function InventoryBox() {
 
     let handlers = useContext(InventoryContext)
 
+    // sort inventory into their types
+    const small = handlers?.inv.filter(i => i.candy_base_type === 'S').sort((a, b) => a.candy_level - b.candy_level)
+    const medium = handlers?.inv.filter(i => i.candy_base_type === 'M').sort((a, b) => a.candy_level - b.candy_level)
+    const large = handlers?.inv.filter(i => i.candy_base_type === 'L').sort((a, b) => a.candy_level - b.candy_level)
+    const cake = handlers?.inv.filter(i => i.candy_base_type === 'C').sort((a, b) => a.candy_level - b.candy_level)
+
+    let candies = [small, medium, large, cake]
+
+    const candiess = {
+        "Small": small,
+        "Medium": medium,
+        "Large": large,
+        "Cake": cake
+    }
+
+
     return (
         <>
             <div className="mini-page">
                 <Card className='tasklist-position'>
                     <Card.Header>
                         <Stack direction="horizontal" gap={3}>
-                            <div className='to-do-header'>
+                            <div className='inventory-header'>
                                 ITEMS
                             </div>
                             <div className="ms-auto">
@@ -32,15 +48,52 @@ function InventoryBox() {
                             </div>
                         </Stack>
                     </Card.Header>
-                    <div className="inventory-wrapper">
-                        {
-                            handlers?.inv.map((candy, id) => {
-                                return <Candy key={id} id={candy.inventory_id} quantity={candy.quantity} candy_base_type={candy.candy_base_type} candy_level={candy.candy_level} />
-                            })
-                        }
-                    </div>
+
+
+
+
+
+
+                    {handlers.inv.length === 0 ?
+
+                        <ListGroup variant="flush">
+                            <ListGroup.Item className="d-flex justify-content-between align-items-start">
+                                No Inventory!
+                            </ListGroup.Item>
+                        </ListGroup>
+
+                        :
+
+                        // All the candies rendered below - logic included to not show if candy lists are empty 
+                        <div className="inventory-wrapper">
+                            {
+                                Object.keys(candiess).map((key, index) => {
+                                    return (
+                                        <>
+                                            {candiess[key].length !== 0 ?
+                                                <div className="space-candy-types">
+                                                    <div className="text-wrapper">{key}</div>
+                                                    <div className="inventory-wrapper-sizes">
+                                                        {candiess[key].map((candy, id) => {
+                                                            return <Candy key={id} id={candy.inventory_id} quantity={candy.quantity} candy_base_type={candy.candy_base_type} candy_level={candy.candy_level} />
+                                                        })}
+                                                    </div>
+                                                    <hr />
+                                                </div>
+                                                :
+                                                <></>
+                                            }
+
+                                        </>
+                                    )
+                                })
+
+                            }
+                        </div>
+
+                    }
                 </Card>
-            </div>
+            </div >
 
 
         </>
