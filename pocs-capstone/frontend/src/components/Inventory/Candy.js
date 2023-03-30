6742b09701f5fe3454cf5e22ead50e23b300feea
jay-newman <tup16252@temple.edu>
2023-03-30 16:07:11 -0400

    try deactivating meta

--- pocs-capstone/frontend/src/components/Inventory/Candy.js
+++ pocs-capstone/frontend/src/components/Inventory/Candy.js
@@ -114,7 +114,7 @@ function Candy({ id, quantity, candy_base_type, candy_level }) {
 
     return (
         <>            
-            <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+            {/* <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta> */}
         
 
 
