6a15006e4a5923842c4e95c8293f6cd38c8a49d5
Katrina Janeczko <tuk04893@temple.edu>
2023-04-03 23:32:54 -0400

    adding console logs to try and figure out why service worker not working

--- pocs-capstone/frontend/src/serviceWorkerRegistration.js
+++ pocs-capstone/frontend/src/serviceWorkerRegistration.js
@@ -12,26 +12,28 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.0/8 are considered localhost for IPv4.
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  //removed from following if statement: 
+  console.log("service worker...?")  
+  if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+    console.log("will now try to register service worker...?")
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
+      console.log("PUBLIC_URL is on a different origin, service worker won't work.")
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
-
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
@@ -41,7 +43,7 @@ export function register(config) {
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://cra.link/PWA'
+            'worker. To learn more, visit https://cra.link/PWA'
           );
         });
       } else {
@@ -54,8 +56,52 @@ export function register(config) {
 
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
-    .register(swUrl)
+    .register(swUrl, { scope: '/' })
     .then((registration) => {
+
+      // Check if push messaging supported
+      if (!('PushManager' in window)) {
+        console.log('Push messaging is not supported');
+        return;
+      }
+
+      // Get push subscription if already available
+      registration.pushManager.getSubscription().then((subscription) => {
+        if (subscription) {
+          console.log('Already subscribed to push notifications');
+        } else {
+          // Request push subscription
+          registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(config.VAPID_PUBLIC_KEY),
+          })
+            .then((newSubscription) => {
+              console.log('New subscription to push notifications:', newSubscription);
+            })
+            .catch((error) => {
+              console.error('Error while subscribing to push notifications:', error);
+            });
+        }
+      });
+
+      // Listen for push notifications
+      registration.pushManager.addEventListener('push', (event) => {
+        console.log('Received push notification', event);
+
+        const title = 'Study Buddy';
+        const options = {
+          body: event.data.text(),
+          icon: '/logo.svg',
+          badge: '/logo.svg',
+          data: {
+            url: isLocalhost ? 'http://localhost:3000/' : 'http://68.183.30.203:3000/', // url to open when notification is clicked
+          },
+        };
+
+        event.waitUntil(registration.showNotification(title, options));
+      });
+
+
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
@@ -69,7 +115,7 @@ function registerValidSW(swUrl, config) {
               // content until all client tabs are closed.
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.'
+                'tabs for this page are closed. See https://cra.link/PWA.'
               );
 
               // Execute callback
@@ -109,6 +155,7 @@ function checkValidServiceWorker(swUrl, config) {
         (contentType != null && contentType.indexOf('javascript') === -1)
       ) {
         // No service worker found. Probably a different app. Reload the page.
+        console.log("valid service worker not found :(")
         navigator.serviceWorker.ready.then((registration) => {
           registration.unregister().then(() => {
             window.location.reload();
@@ -117,6 +164,7 @@ function checkValidServiceWorker(swUrl, config) {
       } else {
         // Service worker found. Proceed as normal.
         registerValidSW(swUrl, config);
+        console.log("valid service worker found!")
       }
     })
     .catch(() => {
@@ -134,4 +182,19 @@ export function unregister() {
         console.error(error.message);
       });
   }
+}
+
+// Utility function to convert key from base64-encoded string to a 
+// Uint8Array needed for subscribing to push notifications
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
