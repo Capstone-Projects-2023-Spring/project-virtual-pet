e89846eb09192d0c23f9c2cec398c08bc8365a2d
Christine Cho <tun66463@temple.edu>
2023-04-06 01:41:17 -0400

    update console.log

--- pocs-capstone/frontend/src/api/axios.js
+++ pocs-capstone/frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_DB_URL
 
-console.log(BASE_URL)
+console.log("DB URL: ", BASE_URL)
 
 export default axios.create({
     baseURL: BASE_URL
