8a479ff99857702f6d41a4536a26f80e8b794c61
Katrina Janeczko <tuk04893@temple.edu>
2023-04-06 23:19:14 -0700

    when user first enters Main, ask for permission

--- pocs-capstone/frontend/src/components/Main.js
+++ pocs-capstone/frontend/src/components/Main.js
@@ -2,7 +2,7 @@ import PetDisplay from "./PetDisplay/PetDisplay.js";
 import PageDisplay from "./PageDisplay/PageDisplay.js";
 //import useAuth from '../hooks/useAuth.js'
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import { useWindowWidth } from "@react-hook/window-size";
 import useAxiosPrivate from "../hooks/useAxiosPrivate.js";
 import "./Main.css";
@@ -16,247 +16,284 @@ import GlobalContext from "../context/GlobalContext.js";
 import CalculateXP from "../algos/assignXP.js";
 import CalculatePetLevel from "../algos/calculatePetLevel.js";
 import { setIn } from "formik";
-
+import { NotificationContext } from '../context/NotificationProvider';
+import { startTaskChecker, checkNotificationPromise, requestNotificationPermission, sendWelcomeNotification, sendTaskDueNotification } from './NotificationUtils';
 
 
 const Main = () => {
-  const axiosPrivate = useAxiosPrivate();
-  const [avatarInfo, setAvatar] = useState({});
-  const width = useWindowWidth();
-  const nav = useNavigate();
+    const axiosPrivate = useAxiosPrivate();
+    const [avatarInfo, setAvatar] = useState({});
+    const width = useWindowWidth();
+    const nav = useNavigate();
 
-  const [ready, setReady] = useState(false);
-  const populateInventory = LockedInv()
-  const [inventory, setInventory] = useState(populateInventory);
-  // Moved from PetDisplay
-  const [level_info, setLevelInfo] = useState(CalculatePetLevel(avatarInfo.total_xp))
-  const [spritesheetInstance, setSpritesheetInstance] = useState(null);
+    const [ready, setReady] = useState(false);
+    const populateInventory = LockedInv()
+    const [inventory, setInventory] = useState(populateInventory);
+    // Moved from PetDisplay
+    const [level_info, setLevelInfo] = useState(CalculatePetLevel(avatarInfo.total_xp))
+    const [spritesheetInstance, setSpritesheetInstance] = useState(null);
+
+    const [permissionState, setPermissionState] = useContext(NotificationContext);
+
+    useEffect(() => {
+        // if a user has not yet granted nor denied permission, prompt them
+        if (permissionState === "default") {
+            console.log("Will now request notification permission from user.");
+            // check if browser supports Notifications
+            if (!("Notification" in window)) {
+                console.log("This browser does not support notifications.");
+                setPermissionState("denied");
+                // check if promise-based version is supported (non-Safari)
+            } else if (checkNotificationPromise()) {
+                Notification.requestPermission().then((permission) => {
+                    console.log("This browser supports notifications (non-Safari).");
+                    setPermissionState(permission);
+                });
+                // if not, run older callback-based version (supported in Safari)
+            } else {
+                Notification.requestPermission((permission) => {
+                    console.log("This browser supports notifications (Safari).");
+                    setPermissionState(permission);
+                });
+            }
+            // once a user has granted permission, send them a welcome notification
+            if (permissionState === 'granted') {
+                sendWelcomeNotification();
+            }
+        }
 
 
-  let spriteSheetRef = useRef(null);
-  useEffect(() => {
-    axiosPrivate
-      .get("/avatar/")
-      .then((response) => {
-        setAvatar(response.data[0]);
-        if (!response.data[0]) nav("/pet_selection");
-        else setReady(true);
-      })
-      .catch((error) => {
-        console.log(error);
-        nav("/pet_selection");
-      });
-  }, []);
+    }, [permissionState, setPermissionState]);
 
-  // useEffect, GET call to retrieve inventory item and set the state of inv
-  useEffect(() => {
-    
-    axiosPrivate
-      .get("/inventory/")
-      .then((r) => {
-        
-        r.data.map((it) => {
-          
-          let candy = inventory.find((c) => c.candy_base_type === it.candy_base_type && c.candy_level === it.candy_level);
+    useEffect(() => {
+        const stopTaskChecker = startTaskChecker(24 * 60 * 60 * 1000);
+        return () => stopTaskChecker();
+      }, []);
 
-          candy.inventory_id = it.inventory_id;
-          candy.quantity = it.quantity;
+    let spriteSheetRef = useRef(null);
+    useEffect(() => {
+        axiosPrivate
+            .get("/avatar/")
+            .then((response) => {
+                setAvatar(response.data[0]);
+                if (!response.data[0]) nav("/pet_selection");
+                else setReady(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                nav("/pet_selection");
+            });
+    }, []);
+
+    // useEffect, GET call to retrieve inventory item and set the state of inv
+    useEffect(() => {
+
+        axiosPrivate
+            .get("/inventory/")
+            .then((r) => {
+
+                r.data.map((it) => {
+
+                    let candy = inventory.find((c) => c.candy_base_type === it.candy_base_type && c.candy_level === it.candy_level);
+
+                    candy.inventory_id = it.inventory_id;
+                    candy.quantity = it.quantity;
 
 
-        })        
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-      
-    // axiosPrivate
-    //   .get("/inventory/")
-    //   .then((response) => {
-    //     setInventory(response.data);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-  }, []);
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
-  const isMobile = width <= 850;
+        // axiosPrivate
+        //   .get("/inventory/")
+        //   .then((response) => {
+        //     setInventory(response.data);
+        //   })
+        //   .catch((error) => {
+        //     console.log(error);
+        //   });
+    }, []);
 
-  // Inventory Handlers
-  // Perform a put to the backend to update inventory
-  const putInventory = (item) => {
-    let request = axiosPrivate.put(`/inventory/${item.inventory_id}/`, item);
-    return request.then((response) => response.data);
-  };
-  // Performs update on candy quantity when candy is fed(drag and dropped)
-  const updateInventory = (id) => {
-    // console.log("ID OF CANDY", id)
-    let yourDate = new Date()
-    console.log("DATE???----->",yourDate.toISOString().split('T')[0])
+    const isMobile = width <= 850;
 
-    const candyD = inventory.find((candy) => candy.inventory_id === id);
-    if (candyD.quantity !== 0) {
-      // console.log("CANDY", candyD)
-      const updateCandy = {
-        ...candyD,
-        quantity: candyD.quantity !== 0 ? candyD.quantity - 1 : 0,
-      };
-      // console.log("ID", updateCandy.inventory_id)
-      putInventory(updateCandy).then((r) => {
-        setInventory(inventory.map((it) => (it.inventory_id === id ? updateCandy : it)));
-      });
+    // Inventory Handlers
+    // Perform a put to the backend to update inventory
+    const putInventory = (item) => {
+        let request = axiosPrivate.put(`/inventory/${item.inventory_id}/`, item);
+        return request.then((response) => response.data);
+    };
+    // Performs update on candy quantity when candy is fed(drag and dropped)
+    const updateInventory = (id) => {
+        // console.log("ID OF CANDY", id)
+        let yourDate = new Date()
+        console.log("DATE???----->", yourDate.toISOString().split('T')[0])
+
+        const candyD = inventory.find((candy) => candy.inventory_id === id);
+        if (candyD.quantity !== 0) {
+            // console.log("CANDY", candyD)
+            const updateCandy = {
+                ...candyD,
+                quantity: candyD.quantity !== 0 ? candyD.quantity - 1 : 0,
+            };
+            // console.log("ID", updateCandy.inventory_id)
+            putInventory(updateCandy).then((r) => {
+                setInventory(inventory.map((it) => (it.inventory_id === id ? updateCandy : it)));
+            });
+        }
+    };
+
+    // Performs a get request for the users inventory
+    const getInventory = () => {
+        let request = axiosPrivate.get("/inventory/");
+        return request.then((response) => response.data);
+    };
+
+    // Performs a POST request to add items
+    const createInventoryItem = (candy) => {
+        const request = axiosPrivate.post("/inventory/", candy);
+        return request.then((response) => response.data);
+    };
+
+    // Performs a DELETE request to remove items
+    const deleteInventoryItem = (candyID) => {
+        const request = axiosPrivate.delete(`/inventory/${candyID}/`);
+        return request.then((response) => response.data);
+    };
+
+    const animateSpriteSheet = () => {
+        // if (spriteSheetRef.current) {
+        spriteSheetRef.current.goToAndPlay(1);
+        spriteSheetRef.current.pause();
+        //  }
+    };
+
+    // Create a bunch of items in the backend and change the state of inv
+    const postFullInventory = () => {
+        if (inventory.length === 0) {
+            let fullInventoryData = PopulateInv();
+            let populatedItems = [];
+            fullInventoryData.forEach((importItem) => {
+                createInventoryItem(importItem).then((r) => {
+                    populatedItems.push(r);
+                    setInventory([...inventory, ...populatedItems]);
+                });
+            });
+        }
+    };
+
+    // Delete all the items in the backend and set the state of inv to []
+    const deleteAll = () => {
+        if (inventory.length) {
+            inventory.forEach((i) => {
+                deleteInventoryItem(i.inventory_id);
+            });
+        }
+
+        setInventory([]);
+    };
+    // Moved from PetDisplay - passed base type and level when called in Candy
+    const getExp = (candy_base_type, candy_level) => {
+
+
+
+        const received_xp = CalculateXP(candy_base_type, candy_level)
+        console.log("XP", avatarInfo.total_xp)
+
+        const total_xp = received_xp + avatarInfo.total_xp
+
+        const today = new Date()
+        const todayString = today.toISOString().split('T')[0]
+
+        console.log("TOTAL XP----------->", total_xp)
+        const updatedAvatar = {
+            ...avatarInfo,
+            total_xp: total_xp,
+            last_feed: todayString
+        };
+        console.log("UPDATED AVATAR", updatedAvatar)
+        axiosPrivate
+            .patch(`/avatar/${avatarInfo.avatar_id}/`, updatedAvatar)
+            .then((response) => {
+                console.log("response.data:", response.data);
+                setAvatar(response.data); //change this to add to previous state instead of replacing completely (in case of >1 avatar for 1 user)
+                getLevel(avatarInfo.total_xp)
+
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    // Moved from PetDisplay
+    const getLevel = (xp) => {
+        setLevelInfo(CalculatePetLevel(xp));
+        // Heart animation. Need to talk to Mary and Alex about this. 
+        spritesheetInstance.goToAndPlay(1);
+        spritesheetInstance.pause();
+
+
     }
-  };
 
-  // Performs a get request for the users inventory
-  const getInventory = () => {
-    let request = axiosPrivate.get("/inventory/");
-    return request.then((response) => response.data);
-  };
+    const handlers = {
+        inventory,
+        setInventory,
+        createInventoryItem,
+        updateInventory,
+        deleteInventoryItem,
+        postFullInventory,
+        deleteAll,
+        avatarInfo,
+        setAvatar,
+        animateSpriteSheet,
+        setSpritesheetInstance,
+        getExp
+    };
 
-  // Performs a POST request to add items
-  const createInventoryItem = (candy) => {
-    const request = axiosPrivate.post("/inventory/", candy);
-    return request.then((response) => response.data);
-  };
+    const animate = {
+        animateSpriteSheet,
+    };
 
-  // Performs a DELETE request to remove items
-  const deleteInventoryItem = (candyID) => {
-    const request = axiosPrivate.delete(`/inventory/${candyID}/`);
-    return request.then((response) => response.data);
-  };
-
-  const animateSpriteSheet = () => {
-    // if (spriteSheetRef.current) {
-    spriteSheetRef.current.goToAndPlay(1);
-    spriteSheetRef.current.pause();
-    //  }
-  };
-
-  // Create a bunch of items in the backend and change the state of inv
-  const postFullInventory = () => {
-    if (inventory.length === 0) {
-      let fullInventoryData = PopulateInv();
-      let populatedItems = [];
-      fullInventoryData.forEach((importItem) => {
-        createInventoryItem(importItem).then((r) => {
-          populatedItems.push(r);
-          setInventory([...inventory, ...populatedItems]);
-        });
-      });
-    }
-  };
-
-  // Delete all the items in the backend and set the state of inv to []
-  const deleteAll = () => {
-    if (inventory.length) {
-      inventory.forEach((i) => {
-        deleteInventoryItem(i.inventory_id);
-      });
+    if (!ready) {
+        return <div>LOADING...</div>;
     }
 
-    setInventory([]);
-  };
-  // Moved from PetDisplay - passed base type and level when called in Candy
-  const getExp = (candy_base_type, candy_level) => {
-    
-
-
-    const received_xp = CalculateXP(candy_base_type, candy_level)
-    console.log("XP", avatarInfo.total_xp)
-
-    const total_xp = received_xp + avatarInfo.total_xp
-    
-    const today = new Date()
-    const todayString = today.toISOString().split('T')[0]
-
-    console.log("TOTAL XP----------->", total_xp)
-    const updatedAvatar = {
-        ...avatarInfo,
-        total_xp:total_xp,
-        last_feed:todayString
-      };
-      console.log("UPDATED AVATAR",updatedAvatar)
-      axiosPrivate
-        .patch(`/avatar/${avatarInfo.avatar_id}/`, updatedAvatar)
-        .then((response) => {
-          console.log("response.data:", response.data);
-          setAvatar(response.data); //change this to add to previous state instead of replacing completely (in case of >1 avatar for 1 user)
-          getLevel(avatarInfo.total_xp)
-            
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-  };
-  // Moved from PetDisplay
-  const getLevel = (xp) => {
-    setLevelInfo(CalculatePetLevel(xp));
-    // Heart animation. Need to talk to Mary and Alex about this. 
-    spritesheetInstance.goToAndPlay(1);
-    spritesheetInstance.pause();
-    
-
-}
-
-  const handlers = {
-    inventory,
-    setInventory,
-    createInventoryItem,
-    updateInventory,
-    deleteInventoryItem,
-    postFullInventory,
-    deleteAll,
-    avatarInfo,
-    setAvatar,
-    animateSpriteSheet,
-    setSpritesheetInstance,
-    getExp,
-  };
-
-  const animate = {
-    animateSpriteSheet,
-  };
-
-  if (!ready) {
-    return <div>LOADING...</div>;
-  }
-
-  // Need to wrap mobile view in Dnd and Inventory Context - Want to talk to Harrsion prior
-  else if (!isMobile) {
-    return (
-      <GlobalContext.Provider value={handlers}>
-      {/*<AvatarContext.Provider value={shareData}>*/}
-          {/*<InventoryContext.Provider value={handlers}>*/}
-            {/*<SpriteSheetContext.Provider value={animate}>*/}
-              <div className="flex-pages">
-                <PetDisplay value={handlers}/>
-                <PageDisplay />
-              </div>
-           {/* </SpriteSheetContext.Provider>*/}
-         {/* </InventoryContext.Provider>*/} 
-      {/*</AvatarContext.Provider>*/}
-      </GlobalContext.Provider>
-
-    );
-  } else {
-    return (
-      <GlobalContext.Provider value={handlers}>
-      {/*<AvatarContext.Provider value={shareData}>*/}
-          {/*<InventoryContext.Provider value={handlers}>*/}
-            {/*<SpriteSheetContext.Provider value={animate}>*/}
-              <div>
+    // Need to wrap mobile view in Dnd and Inventory Context - Want to talk to Harrsion prior
+    else if (!isMobile) {
+        return (
+            <GlobalContext.Provider value={handlers}>
+                {/*<AvatarContext.Provider value={shareData}>*/}
+                {/*<InventoryContext.Provider value={handlers}>*/}
+                {/*<SpriteSheetContext.Provider value={animate}>*/}
                 <div className="flex-pages">
-                  <PetDisplay value={handlers}/>
+                    <PetDisplay value={handlers} />
+                    <PageDisplay />
                 </div>
+                {/* </SpriteSheetContext.Provider>*/}
+                {/* </InventoryContext.Provider>*/}
+                {/*</AvatarContext.Provider>*/}
+            </GlobalContext.Provider>
+
+        );
+    } else {
+        return (
+            <GlobalContext.Provider value={handlers}>
+                {/*<AvatarContext.Provider value={shareData}>*/}
+                {/*<InventoryContext.Provider value={handlers}>*/}
+                {/*<SpriteSheetContext.Provider value={animate}>*/}
                 <div>
-                  <PageDisplay />
+                    <div className="flex-pages">
+                        <PetDisplay value={handlers} />
+                    </div>
+                    <div>
+                        <PageDisplay />
+                    </div>
                 </div>
-              </div>
-           {/* </SpriteSheetContext.Provider>*/}
-         {/* </InventoryContext.Provider>*/} 
-      {/*</AvatarContext.Provider>*/}
-      </GlobalContext.Provider>
-    );
-  }
+                {/* </SpriteSheetContext.Provider>*/}
+                {/* </InventoryContext.Provider>*/}
+                {/*</AvatarContext.Provider>*/}
+            </GlobalContext.Provider>
+        );
+    }
 };
 export default Main;
