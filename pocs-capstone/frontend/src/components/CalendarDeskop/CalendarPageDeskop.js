18355d7a097be4b08b274aab5b780b67321529a5
jay-newman <tup16252@temple.edu>
2023-04-11 00:02:07 -0400

    Desktop Calendar. Issue with cascading css from mobile calendar. Was able to shrink calendar for now.

---
+++ pocs-capstone/frontend/src/components/CalendarDeskop/CalendarPageDeskop.js
@@ -0,0 +1,41 @@
+import {useEffect, useState, useContext} from 'react';
+import "./CalendarPageDesktop.css";
+import Calendar from 'react-calendar'; 
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import CalendarTaskList from './CalendarTaskList';
+
+const CalendarPageDesktop = () => {
+
+    const [date, setDate] = useState();
+    const [show, setShow] = useState(false);
+    const [showCreateTask, setShowCreateTask] = useState(false);
+
+    const handleClose = () =>  {
+        setShowCreateTask(false); 
+        setShow(false);
+    }
+    const handleShow = () => setShowCreateTask(true);
+
+    const myf = (v) => {
+        
+        setShow(true);
+        handleShow();
+        
+        setDate(v);
+    }
+
+    return(
+        // <div className="calendar-page">
+
+        <div className="calendar-container">
+            <Calendar onClickDay={(value, event) => myf(value)} />
+
+             {show === true ? <CalendarTaskList {...{ showCreateTask, handleClose, date}}/> : ""}
+            
+        </div>
+
+        
+    )
+}
+
+export default CalendarPageDesktop;
