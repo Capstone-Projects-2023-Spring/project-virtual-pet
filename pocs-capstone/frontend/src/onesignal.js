9993c39b287f75db9008097d285fd66787fcd2db
Katrina Janeczko <tuk04893@temple.edu>
2023-04-14 15:13:05 -0400

    try to not initialize if already initialized

--- pocs-capstone/frontend/src/onesignal.js
+++ pocs-capstone/frontend/src/onesignal.js
@@ -1,10 +1,10 @@
 import OneSignal from 'react-onesignal';
 
-export default async function runOneSignal() {
-    window.OneSignal = window.OneSignal || [];
+window.OneSignal = window.OneSignal || [];
 
+export default async function runOneSignal() {
     if (!OneSignal.initialized) {
-        console.log("Initializing OneSignal: ", OneSignal.initialized)
+        console.log("Initializing OneSignal: ", window.OneSignal)
         await OneSignal.init({
             appId: '03d68522-7024-4cff-a04b-8f49eb789abe',
             safari_web_id: "web.onesignal.auto.4da9f50a-e5cd-4bfe-8999-da1af6f61d49",
