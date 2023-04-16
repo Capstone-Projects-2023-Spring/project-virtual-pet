50a7d2dfdda09cea22060be561f4fac02462622d
Katrina Janeczko <tuk04893@temple.edu>
2023-04-16 15:50:29 -0400

    add timestamp/version to account page

--- pocs-capstone/frontend/src/components/AccountPage.js
+++ pocs-capstone/frontend/src/components/AccountPage.js
@@ -85,91 +85,92 @@ function AccountPage() {
           <div className='account-profile-header'>Account Profile</div>
         </Card.Header>
         <div className="mx-auto">
-        <Form
-          className="form-content"
-          noValidate
-          validated={validated}
-          onSubmit={handleSubmit}
-        >
+          <Form
+            className="form-content"
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+          >
 
-          <Form.Group className="mb-2">
-            <Form.Label>Username:</Form.Label>
-            <Col>
-              <Form.Control type="text" value={username} disabled />
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Username:</Form.Label>
+              <Col>
+                <Form.Control type="text" value={username} disabled />
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>Email:</Form.Label>
-            <Col>
-              <Form.Control type="email" value={email} disabled />
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Email:</Form.Label>
+              <Col>
+                <Form.Control type="email" value={email} disabled />
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2" controlId="validationCustom03">
-            <Form.Label>Birthday:</Form.Label>
-            <Col>
-              <Form.Control
-                required
-                type="date"
-                value={birthday}
-                onChange={handleBirthdayChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide your birthday.
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2" controlId="validationCustom03">
+              <Form.Label>Birthday:</Form.Label>
+              <Col>
+                <Form.Control
+                  required
+                  type="date"
+                  value={birthday}
+                  onChange={handleBirthdayChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide your birthday.
+                </Form.Control.Feedback>
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2" controlId="validationCustom01">
-            <Form.Label>Name:</Form.Label>
-            <Col>
-              <Form.Control
-                required
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide your name.
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2" controlId="validationCustom01">
+              <Form.Label>Name:</Form.Label>
+              <Col>
+                <Form.Control
+                  required
+                  type="text"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide your name.
+                </Form.Control.Feedback>
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2" controlId="validationCustom02">
-            <Form.Label>Bio:</Form.Label>
-            <Col>
-              <Form.Control
-                required
-                as="textarea"
-                rows={3}
-                value={bio}
-                onChange={handleBioChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a bio.
-              </Form.Control.Feedback>
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2" controlId="validationCustom02">
+              <Form.Label>Bio:</Form.Label>
+              <Col>
+                <Form.Control
+                  required
+                  as="textarea"
+                  rows={3}
+                  value={bio}
+                  onChange={handleBioChange}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a bio.
+                </Form.Control.Feedback>
+              </Col>
+            </Form.Group>
 
-          <Form.Group className="mb-2">
-            <Form.Label>
-              Studying Since:
-            </Form.Label>
-            <Col>
-              <Form.Control type="date" value={joinDate} disabled />
-            </Col>
-          </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>
+                Studying Since:
+              </Form.Label>
+              <Col>
+                <Form.Control type="date" value={joinDate} disabled />
+              </Col>
+            </Form.Group>
 
-          <div className="submit-button">
-            <Button
-              style={{ marginTop: '20px', marginBottom: '20px'}}
-              type="submit">Submit</Button>
-          </div>
+            <div className="submit-button">
+              <Button
+                style={{ marginTop: '20px', marginBottom: '20px' }}
+                type="submit">Submit</Button>
+            </div>
 
-        </Form>
+          </Form>
         </div>
 
+        <p>Site version as of 3:48pm 4/16/23</p>
       </Card>
     </div>
   );
