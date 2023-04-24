91dfead33615e521982dc932c56560f8bef94ce0
Katrina Janeczko <tuk04893@temple.edu>
2023-04-24 19:53:37 -0400

    Revert "modify bell icon"
    This reverts commit fb671887461d42769b1aea4da21756c00b7b760f.

--- pocs-capstone/frontend/src/index.js
+++ pocs-capstone/frontend/src/index.js
@@ -29,35 +29,17 @@ serviceWorkerRegistration.register();
 window.OneSignal = window.OneSignal || [];
 OneSignal.push(function () {
   OneSignal.init({
-    appId: '03d68522-7024-4cff-a04b-8f49eb789abe',
+    appId: "03d68522-7024-4cff-a04b-8f49eb789abe",
     safari_web_id: "web.onesignal.auto.4da9f50a-e5cd-4bfe-8999-da1af6f61d49",
-    allowLocalhostAsSecureOrigin: true, // allows HTTPS testing with localhost
-    persistNotification: false, // on non-mobile Chrome, the notification will not go away automatically unless this is false
-    notifyButton: { // subscription bell in lower right corner; goes away after subscribing
+    notifyButton: {
       enable: true,
-      showCredit: true,
-      displayPredicate: () => OneSignal.isPushNotificationsEnabled().then(isEnabled => isEnabled ? false : true),
-      prenotify: true,
-      text: {
-        'message.prenotify': 'Click to subscribe to notifications!',
-      },
-      colors: { // Customize the colors of the main button and dialog popup button
-        'circle.background': 'rgb(255, 198, 198)',
-        'circle.foreground': 'rgb(215, 60, 60)',
-        'badge.background': 'rgb(255, 198, 198)',
-        'badge.foreground': 'rgb(215, 60, 60)',
-        'badge.bordercolor': 'rgb(215, 60, 60)',
-        'pulse.color': 'rgb(217, 91, 91)',
-        'dialog.button.background.hovering': 'rgb(77, 101, 113)',
-        'dialog.button.background.active': 'rgb(70, 92, 103)',
-        'dialog.button.background': 'rgb(255, 198, 198)',
-        'dialog.button.foreground': 'white'
-      },
     },
+    allowLocalhostAsSecureOrigin: true,
+    persistNotification: false, // on non-mobile Chrome, the notification will not go away automatically unless this is false
     welcomeNotification: {
       title: "Welcome to Study Buddy",
       message: "Thanks for subscribing to push notifications!",
-    }
+    },
   });
   OneSignal.getUserId().then(playerId => {
     //Send the playerId to your server to associate it with the user
