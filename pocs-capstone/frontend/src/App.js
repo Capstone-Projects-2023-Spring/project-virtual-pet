8baf543a79bafd7018f847a663f077cf844e7b0c
Katrina Janeczko <tuk04893@temple.edu>
2023-04-25 22:02:50 -0400

    please. please. pleaseeee.

--- pocs-capstone/frontend/src/App.js
+++ pocs-capstone/frontend/src/App.js
@@ -17,35 +17,46 @@ import PersistLogin from './components/LoginLogout/PersistLogin.js'
 import RequireAuth from './components/LoginLogout/RequireAuth.js'
 
 import AnimateChoice from './components/AnimateChoice.js'
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css'
 import CanvasIntegrationPage from './components/CanvasIntegrationPage.js'
 import AccountPage from './components/AccountPage'
 import PetProfPage from './components/PageDisplay/PetProfPage.js'
 
-function App(){
+import OneSignal from 'react-onesignal';
 
+function App() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "03d68522-7024-4cff-a04b-8f49eb789abe",
+      safari_web_id: "web.onesignal.auto.4da9f50a-e5cd-4bfe-8999-da1af6f61d49",
+    });
+    OneSignal.getUserId().then(playerId => {
+      //Send the playerId to your server to associate it with the user
+      console.log("Here is the user's device/browser id:", playerId);
+    })
+  }, []);
   return (
 
     <Routes>
-        <Route path="login" element={<Login/>}/>
-        <Route path="register" element={<Register/>}/>
-        <Route path="logout" element={<Logout/>}/>
-        <Route path ="landing" element={<LandingPage/>}/>
-        <Route element={<PersistLogin />}>
-          <Route element = {<RequireAuth/>}>
-            <Route path="/" element = {<Layout/>}>
-            <Route path="/" element={<Main/>}/>
-            <Route path = "/loader" element={<Loader/>}/>
-            <Route path = "/pet_selection" element ={<AnimateChoice/>}/>
-            <Route path = "/canvas_integration" element = {<CanvasIntegrationPage/>}/>
-            <Route path = "/account" element={<AccountPage/>}/>
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="logout" element={<Logout />} />
+      <Route path="landing" element={<LandingPage />} />
+      <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/loader" element={<Loader />} />
+            <Route path="/pet_selection" element={<AnimateChoice />} />
+            <Route path="/canvas_integration" element={<CanvasIntegrationPage />} />
+            <Route path="/account" element={<AccountPage />} />
             {/* <Route path = "/petprofile" element={<CanvasIntegrationPage/>}/> */}
           </Route>
         </Route>
-      
-      
+
+
       </Route>
     </Routes>
 
--- pocs-capstone/frontend/src/index.js
+++ pocs-capstone/frontend/src/index.js
@@ -26,26 +26,6 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
 
-window.OneSignal = window.OneSignal || [];
-OneSignal.push(function () {
-  OneSignal.init({
-    appId: "03d68522-7024-4cff-a04b-8f49eb789abe",
-    safari_web_id: "web.onesignal.auto.4da9f50a-e5cd-4bfe-8999-da1af6f61d49",
-    notifyButton: {
-      enable: true,
-    },
-    allowLocalhostAsSecureOrigin: true,
-    persistNotification: false, // on non-mobile Chrome, the notification will not go away automatically unless this is false
-    welcomeNotification: {
-      title: "Welcome to Study Buddy",
-      message: "Thanks for subscribing to push notifications!",
-    },
-  });
-  OneSignal.getUserId().then(playerId => {
-    //Send the playerId to your server to associate it with the user
-    console.log("Here is the user's device/browser id:", playerId);
-  });
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
