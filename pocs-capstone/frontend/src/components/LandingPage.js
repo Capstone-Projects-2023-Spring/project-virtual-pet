7c5d2e6136874a98cba192ae8f8fef07e165339f
Katrina Janeczko <tuk04893@temple.edu>
2023-04-26 00:08:20 -0400

    edit landing page about for grammar/capitalization

--- pocs-capstone/frontend/src/components/LandingPage.js
+++ pocs-capstone/frontend/src/components/LandingPage.js
@@ -44,14 +44,13 @@ const LandingPage = () => {
     return (
         <div className="landingpage" style={{width: width}}>
         <h1><center>WELCOME TO STUDY BUDDY!</center></h1>
-        <p>Deployed 2:50pm on 4/24 from Kat's notification branch</p>
         <img src={kittycat} alt="cat" className='image' />
        <center><Card className = "card" style={{width: '30rem'}}>
             <Card.Title>About</Card.Title>
             <Card.Body>
-                Study buddy allows you to take care of a virtual pet to achieve your study goals! Choose from a variety of pets and colors
-                of pets to find the buddy you like best. By completing tasks, you can receive candies of various sizes to feed your pet and watch it grow. 
-                Integrate your institution Canvas account to have your pet keep track of your school assignments. With Study Buddy, you get a cute 
+                Study Buddy allows you to take care of a virtual pet to achieve your academic goals! Choose from a variety of pets and colors
+                of pets to find the buddy who suits you best. By completing tasks, you can receive candies of various sizes to feed your pet. Watch it grow and level up! 
+                Integrate your Canvas account so your pet can help you keep track of your school assignments. With Study Buddy, you get a cute 
                 way to make keeping up with your schoolwork and personal study goals fun! Click on the buttons below to register or log in to see your buddy.
             </Card.Body>
            
